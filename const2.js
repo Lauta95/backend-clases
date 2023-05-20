721a1af4b55c167f5d3dc57aecc54e667a0eccf1
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-05-20 14:38:58 -0300

    const2

---
+++ const2.js
@@ -0,0 +1,9 @@
+const person = {
+    name: 'Dario',
+    apellido: 'Lopez',
+    edad: '56' 
+}
+// se pueden mutar los objetos. es deir se pueden cambiar los valores internos de sus elementos.
+
+person.edad= 30
+console.log(person);
