4cd75dcd2c3ce7e9df99feefa3e5f79b0dfe7e72
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-07-09 15:55:25 -0300

    enviando mensajes desde el front y desde el back con web sockets

--- clase06/src/app.js
+++ clase06/src/app.js
@@ -1,12 +1,27 @@
 import express from 'express'
-import __dirname from './utlis'
+import { Server } from 'socket.io'
+import __dirname from './utlis.js'
+
 
 const app = express()
 
 app.use('/static', express.static(__dirname + '/public'))
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('<h1>hola</h1>')
 })
 
-app.listen(8080)
+// va a ser nuestra caracteristica de nuestro servidor http
+const httpServer = app.listen(8080)
+// para crear un socket basado en este http
+const socketServer = new Server(httpServer)
+
+socketServer.on('connection', socket => {
+    console.log('nuevo cliente conectado');
+
+    socket.on('message', data=>{
+        console.log(data);
+
+        socket.emit('message_ind', 'mensaje recibido desde el backend')
+    })
+})
