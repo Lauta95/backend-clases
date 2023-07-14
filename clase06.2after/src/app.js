d36d8cfe1658b8abf33f69c2e92e96dae65cbc5a
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-07-14 16:47:32 -0300

    se aplica handlebars

--- clase06.2after/src/app.js
+++ clase06.2after/src/app.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import handlebars from 'express-handlebars'
-import { Server } from 'socket.io'
 import productRouter from './routes/product.router.js'
 import cartRouter from './routes/cart.router.js'
 import viewsRouter from './routes/views.router.js'
@@ -19,11 +18,4 @@ app.use('/', viewsRouter)
 app.use('/api/products', productRouter)
 app.use('/api/carts', cartRouter)
 
-const httpServer = app.listen(8080)
-const io = new Server(httpServer)
-
-io.on('connection', socket => {
-    socket.on('new-product', data => {
-        console.log(data)
-    })
-})
+app.listen(8080)
