aaf00a62ac8913f5277640cd2a32df32e44a58fb
Lauta95 <lautaro95ss@gmail.com>
2023-07-24 19:56:26 -0300

    routers

--- clase08_integrador/src/routes/pokemon.router.js
+++ clase08_integrador/src/routes/pokemon.router.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import pokeModel from '../models/pokemon.model.js';
 
 const router = Router();
 
@@ -6,10 +7,19 @@ const router = Router();
 router.get('/', async (req, res) => {
     res.render('list', {})
 })
-// pagina para crear
-
+// pagina para crear(render html) trae el formulario
+router.get('/create', async (req, res) => {
+    res.render('create', {})
+})
+// crear pokemon POST:
+router.post('/create', async(req,res)=>{
+    // 
+})
 // crear uno
-
+router.get('/:name', async (req, res) => {
+    const name = req.params.name
+    res.render('one', {name: 'pikachu'})
+})
 // obtener uno con su nombre
 
 // borrar
